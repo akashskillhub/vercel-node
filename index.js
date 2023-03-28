258c646b0d1d8ee6f72bf12d973fd9967c93163a
akash <akash@gmail.com>
2023-03-28 15:58:34 +0530

    env change

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use("*", (req, res) => {
     })
 })
 app.use(errorHandler)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.VERCEL_PORT || 5000
 
 mongoose.connection.once("open", () => {
     app.listen(PORT, console.log(`SEVER RUNNING http://localhost:${PORT} `))
